a0d4a2fedfef4c3cd89b52197bdb9931dffb72c7
condit <devnull@localhost>
2013-08-30 12:14:10 -0700

    Removed unused imports

--- src/main/java/edu/sdsc/solr/lemmatization/LemmatizationWriter.java
+++ src/main/java/edu/sdsc/solr/lemmatization/LemmatizationWriter.java
@@ -2,10 +2,8 @@ package edu.sdsc.solr.lemmatization;
 
 import static com.google.common.collect.Lists.newArrayList;
 
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStreamWriter;
